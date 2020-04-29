d716684ef8383aa96d3ea64e9bc0f14b38eac836
Albert Khamidullin <albert.khm@gmail.com>
2020-04-29 13:09:13 +0300

    sticky header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {useEffect, useState} from "react";
+import SomeRandomText from "./SomeRandomText";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const fixedText = "I am fixed :)";
+    const whenNotFixed = "Main header";
+
+
+    const [headerText, setHeaderText] = useState(whenNotFixed);
+    useEffect(() => {
+        const header = document.getElementById("myHeader");
+        const sticky = header.offsetTop;
+        const scrollCallBack = window.addEventListener("scroll", () => {
+            if (window.pageYOffset > sticky) {
+                header.classList.add("sticky");
+                if (headerText !== fixedText) {
+                    setHeaderText(fixedText);
+                }
+            } else {
+                header.classList.remove("sticky");
+                if (headerText !== whenNotFixed) {
+                    setHeaderText(whenNotFixed);
+                }
+            }
+        });
+        return () => {
+            window.removeEventListener("scroll", scrollCallBack);
+        };
+    }, []);
+
+    return (
+        <div>
+            <header className="mainHeader header">HEADER</header>
+            <SomeRandomText/>
+            <header id="myHeader" className="header">
+                {headerText}
+            </header>
+            <SomeRandomText/>
+        </div>
+    );
 }
 
 export default App;
